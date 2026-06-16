8558fb29f0714bcd065cfd406630a6618903492e
synchromic <realvince7778@gmail.com>
2026-06-16 13:49:58 -0700

    main page layout

--- src/lib/components/TitleBox.svelte
+++ src/lib/components/TitleBox.svelte
@@ -4,27 +4,34 @@
 	let { children, title }: { children: Snippet; title: string } = $props();
 </script>
 
-<section class="container">
-	<h2 class="title">{title}</h2>
+<section>
+	<header>
+		<h2>{title}</h2>
+	</header>
 	{@render children()}
 </section>
 
 <style>
-	.container {
-		width: 500px;
+	section {
 		border: 1px solid var(--foreground-color);
+		background-color: var(--background-color);
 	}
 
-	.title {
-		display: block;
+	header {
 		margin: 0;
-		padding: 0.2em;
+		padding: 0.3em;
 		border-bottom: 1px solid var(--foreground-color);
 
 		background-color: var(--background-color-l);
+	}
 
-		font-family: "Roboto Mono", Consolas, monaco, monospace;
+	h2 {
+		margin: 0;
+
+		font-family: "Source Code Pro", Consolas, monaco, monospace;
 		font-size: 1em;
+		font-weight: normal;
 		text-transform: uppercase;
+		letter-spacing: 0.08em;
 	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,55 @@
 	import TitleBox from "$lib/components/TitleBox.svelte";
 </script>
 
-<h1>synchromic</h1>
+<div class="container">
+	<header>
+		<h1>synchromic</h1>
+	</header>
 
-<TitleBox title="Quick Links">
-	<p>Hello, world!</p>
-</TitleBox>
+	<main>
+		<TitleBox title="Quick Links">
+			<p>Hello, world!</p>
+		</TitleBox>
+		<TitleBox title="Latest Blog Posts">
+			<p>Hello, world!</p>
+		</TitleBox>
+		<TitleBox title="Featured Project">
+			<p>Hello, world!</p>
+		</TitleBox>
+		<TitleBox title="Random Cat Photo">
+			<p>Hello, world!</p>
+		</TitleBox>
+	</main>
+</div>
+
+<style>
+	.container {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		height: 100%;
+		justify-content: center;
+	}
+
+	header {
+		margin-bottom: 1em;
+		padding: 1em;
+
+		border: 1px solid var(--foreground-color);
+		background-color: var(--background-color);
+		font-weight: bold;
+	}
+
+	h1 {
+		margin: 0;
+
+		font-family: "Source Code Pro", Consolas, monaco, monospace;
+		letter-spacing: 0.1em;
+	}
+
+	main {
+		display: grid;
+		grid-template-columns: 500px 500px;
+		gap: 1em;
+	}
+</style>
